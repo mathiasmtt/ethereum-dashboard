763ecd28cbf94f5461fc3f664c03179cccad3819
Mathias <31.mathias@gmail.com>
2024-06-11 15:13:10 -0300

    actualizacion

--- ethereum/urls.py
+++ ethereum/urls.py
@@ -1,9 +1,12 @@
-# ethereum/urls.py
-
 from django.urls import path
-from .views import address_list, dashboard
+from .views import register, login_view, address_list, dashboard, home
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', address_list, name='address_list'),
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('', home, name='home'),
+    path('addresses/', address_list, name='address_list'),
     path('dashboard/', dashboard, name='dashboard'),
 ]
--- ethereum/views.py
+++ ethereum/views.py
@@ -29,3 +29,36 @@ def dashboard(request):
 
 def home(request):
     return render(request, 'ethereum/home.html')
+
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'ethereum/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'ethereum/login.html', {'form': form})
